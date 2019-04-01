f58e8aa7e7aa2cf7faadac7be05fc8ab18120d46
Thomas Aven <avensin@hotmail.com>
2019-04-01 13:44:42 +0200

    Set new default resolution.

--- gloom/src/gloom/gloom.hpp
+++ gloom/src/gloom/gloom.hpp
@@ -10,8 +10,8 @@
 #include <string>
 
 // Constants
-const int         windowWidth     = 1024;
-const int         windowHeight    = 768;
+const int         windowWidth     = 1600;
+const int         windowHeight    = 900;
 const std::string windowTitle     = "OpenGL";
 const GLint       windowResizable = GL_FALSE;
 const int         windowSamples   = 4;
